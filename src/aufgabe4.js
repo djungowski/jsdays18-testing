d33caee29df153c7d4fca5693b885e82d6b2d5ef
Sebastian Springer <info@sebastian-springer.com>
2018-10-11 15:49:17 +0200

    block all the things

--- src/aufgabe4.js
+++ src/aufgabe4.js
@@ -1,5 +1,11 @@
 function async(cb, value = '', timeout = 0) {
   setTimeout(() => {
+    const t = Date.now();
+    // while(true) {
+    //   if (Date.now() - t > 5000) {
+    //     break;
+    //   }
+    // }
     cb(value);
   }, timeout);
 }
