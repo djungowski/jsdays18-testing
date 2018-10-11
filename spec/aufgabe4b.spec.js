5f6165472e92d6f4d6375c4a4c643db4b456e771
Sebastian Springer <info@sebastian-springer.com>
2018-10-11 14:31:06 +0200

    4b

--- spec/aufgabe4b.spec.js
+++ spec/aufgabe4b.spec.js
@@ -8,4 +8,19 @@ describe('getRandomNumber4b', () => {
     getRandomNumber();
     expect(Math.random).toHaveBeenCalled();
   });
+  it('should return a number and call Math.random', () => {
+    // gib einen definierten Wert zurück
+    //spyOn(Math, 'random').and.returnValue(0.2);
+    // ruf die originalfunktion auf
+    //spyOn(Math, 'random').and.callThrough();
+    // ruf eine andere Funktion auf
+    //spyOn(Math, 'random').and.callFake(() => 0.1);
+
+    const spy = spyOn(Math, 'random');
+    spy.and.callFake(() => 0.1);
+
+    const result = getRandomNumber();
+    expect(Math.random).toHaveBeenCalled();
+    expect(result).toEqual(jasmine.any(Number));
+  });
 });
